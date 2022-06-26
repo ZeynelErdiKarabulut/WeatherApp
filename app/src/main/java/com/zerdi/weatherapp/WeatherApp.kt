020a42b5df8fd71295470caa8ca999b881220a4b
Erdi Karabulut <tpekarabulut@architecht.com>
2022-06-26 23:16:40 +0300

    Initial commit

---
+++ app/src/main/java/com/zerdi/weatherapp/WeatherApp.kt
@@ -0,0 +1,7 @@
+package com.zerdi.weatherapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApp: Application()
---
+++ app/src/main/java/com/zerdi/weatherapp/di/LocationModule.kt
@@ -0,0 +1,20 @@
+package com.zerdi.weatherapp.di
+
+import com.zerdi.weatherapp.data.location.DefaultLocationTracker
+import com.zerdi.weatherapp.domain.location.LocationTracker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
